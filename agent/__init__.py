d38a4cd2fe5a601f3e86a413c2b94335a1be7b57
yanchen-32 <zhangshuyi320@gmail.com>
2026-08-28 15:21:03 +0800

    feat: export B1 and runtime benchmark agents

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,13 +1,19 @@
-from .base import Agent, LLMClient
+from .base import Agent, LLMClient, estimate_tokens, select_context_indices
 from .clients import OpenAICompatibleClient, RuleBasedClient
+from .full_history import FullHistoryAgent
 from .no_memory import NoMemoryAgent
+from .runtime_agent import MemoryRuntimeAgent
 from .vector_agent import VectorMemoryAgent
 
 __all__ = [
     "Agent",
     "LLMClient",
+    "estimate_tokens",
+    "select_context_indices",
     "OpenAICompatibleClient",
     "RuleBasedClient",
     "NoMemoryAgent",
+    "FullHistoryAgent",
     "VectorMemoryAgent",
+    "MemoryRuntimeAgent",
 ]
